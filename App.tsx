cd425923727a189156bf0095e6cb70eb7e76c8fb
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 13:08:21 +0530

    added function to create Password

--- App.tsx
+++ App.tsx
@@ -18,7 +18,14 @@ export default function App() {
   const [symbols, setSymbols] = useState(false);
 
   const generatePassword = (passwordLength: number) => {};
-  const createPassword = (characters: string, passwordLength: number) => {};
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = '';
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.floor(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  };
   const resetPasswordState = () => {};
 
   return (
