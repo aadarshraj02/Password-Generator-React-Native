ca37abb6b6ef0272317df5a5d07a398ebe5934fd
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 12:45:22 +0530

    added validation for password

--- App.tsx
+++ App.tsx
@@ -1,5 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Minimum 4 characters required')
+    .max(16, 'Max 16 characters required')
+    .required('Length is required'),
+});
 
 export default function App() {
   return (
