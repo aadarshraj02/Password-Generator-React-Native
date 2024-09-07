f9317e85945f36cf05f7a03314f48bca3be6d16f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-07 12:53:44 +0530

    added states for password,uppercase,lowercase,passwords and symbold

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import * as Yup from 'yup';
 
 const PasswordSchema = Yup.object().shape({
@@ -10,6 +10,13 @@ const PasswordSchema = Yup.object().shape({
 });
 
 export default function App() {
+  const [password, setPassword] = useState('');
+  const [isPassGenerated, setIsPassGenerated] = useState(false);
+  const [lowerCase, setLowerCase] = useState(true);
+  const [upperCase, setUpperCase] = useState(false);
+  const [numbers, setNumbers] = useState(false);
+  const [symbols, setSymbols] = useState(false);
+
   return (
     <View>
       <Text>App</Text>
